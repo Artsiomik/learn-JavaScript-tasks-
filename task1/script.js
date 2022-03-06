833ef2b49c17a0ffb426acd374ce0f73017f513f
Artsiom <Artyom.artyom.87@bk.ru>
2022-03-06 15:24:33 +0300

    solution

---
+++ task1/script.js
@@ -0,0 +1,7 @@
+
+
+//Используйте запись в квадратных скобках, чтобы найти первый символ в lastName переменной и присвоить его firstLetterOfLastName.
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+firstLetterOfLastName = lastName[0];
